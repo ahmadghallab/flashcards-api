dc760fc038c750eafa8ad88d7f5b4ea92f618322
Ahmad Ghallab <help.ahmadghallab@gmail.com>
2019-04-13 21:11:40 +0200

    Allow any permission

--- cards/views.py
+++ cards/views.py
@@ -4,9 +4,11 @@ from . import models
 from . import serializers
 
 class ListCreateStudySet(generics.ListCreateAPIView):
+    permission_classes = (permissions.AllowAny,)
     queryset = models.StudySet.objects.all()
     serializer_class = serializers.StudySetSerializer
 
 class RetrieveUpdateDestroyStudySet(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.AllowAny,)
     queryset = models.StudySet.objects.all()
     serializer_class = serializers.StudySetSerializer
