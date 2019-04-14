ae352a634a8528aa49da28d9049b5076c893aa4f
Ahmad Ghallab <help.ahmadghallab@gmail.com>
2019-04-14 08:35:45 +0200

    Removing allow any permission

--- cards/views.py
+++ cards/views.py
@@ -1,15 +1,12 @@
 from rest_framework import generics
-from rest_framework import permissions
 
 from . import models
 from . import serializers
 
 class ListCreateStudySet(generics.ListCreateAPIView):
-    permission_classes = (permissions.AllowAny,)
     queryset = models.StudySet.objects.all()
     serializer_class = serializers.StudySetSerializer
 
 class RetrieveUpdateDestroyStudySet(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (permissions.AllowAny,)
     queryset = models.StudySet.objects.all()
     serializer_class = serializers.StudySetSerializer
