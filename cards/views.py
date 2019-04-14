ceccc2bd30678c9a4b6a3cea1f3b384a34db3f3e
Ahmad Ghallab <help.ahmadghallab@gmail.com>
2019-04-14 18:55:36 +0200

    Fixing order_by

--- cards/views.py
+++ cards/views.py
@@ -4,7 +4,7 @@ from . import models
 from . import serializers
 
 class ListCreateStudySet(generics.ListCreateAPIView):
-    queryset = models.StudySet.objects.all()
+    queryset = models.StudySet.objects.all().order_by('-id')
     serializer_class = serializers.StudySetSerializer
 
 class RetrieveUpdateDestroyStudySet(generics.RetrieveUpdateDestroyAPIView):
@@ -18,7 +18,7 @@ class ListCreateCard(generics.ListCreateAPIView):
     def get_queryset(self):
         return self.queryset.filter(
             studyset_id=self.kwargs.get('studyset_id')
-        ).order_by('-order')
+        ).order_by('-id')
 
 class RetrieveUpdateDestroyCard(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Card.objects.all()
