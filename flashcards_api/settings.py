1a3841a08cb24b28b5689466407cba33b2fa3a8b
Ahmad Ghallab <help.ahmadghallab@gmail.com>
2019-04-21 16:10:06 +0200

    settings.py update

--- flashcards_api/settings.py
+++ flashcards_api/settings.py
@@ -128,7 +128,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'dist')
+STATIC_URL = '/dist/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'dist'),
+]
+
+STATICFILES_FINDERS = ( 
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
 
 AUTH_USER_MODEL = 'accounts.User'
 
