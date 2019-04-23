876f97b1176bf011210aa1310caf3d72e98d6292
Ahmad Ghallab <help.ahmadghallab@gmail.com>
2019-04-23 07:24:05 +0200

    catch all urls to index.html

--- flashcards_api/urls.py
+++ flashcards_api/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
     path('api/v1/', include('cards.urls', namespace='apiv1')),
     path('api/v1/accounts/', include('accounts.urls', namespace='accounts')),
+    url(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
